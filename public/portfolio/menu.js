981581c0bd152e782fef02afdd3077b04c652428
Hong, Chang Nam <hcn1519@gmail.com>
2017-10-28 02:37:04 +0900

    scroll menu

--- public/portfolio/menu.js
+++ public/portfolio/menu.js
@@ -18,3 +18,19 @@ function selectMenu(i) {
     break;
   }
 }
+
+window.onscroll = function() {
+    var menu = document.getElementById('menu');
+
+    var menuOffset1 = document.getElementById("menu1").offsetTop
+    var menuOffset2 = document.getElementById("menu2").offsetTop
+    var menuOffset3 = document.getElementById("menu3").offsetTop
+
+    if (window.pageYOffset <= 680) {
+      selectMenu(1);
+    } else if (window.pageYOffset > 680 && window.pageYOffset < 3500) {
+      selectMenu(2);
+    } else {
+      selectMenu(3);
+    }
+}
